abcac01e08667618f5f856332b4f4eaf7fdbd8d2
Moises David Higuerey Hernandez <hhmoises@hotmail.com>
2019-05-04 13:28:21 -0600

    Tabla hash completada. Funcion de insertar completada. Funcion de eliminar completada. Funcion de modificar completada. Funcion de consultar completada. Implementacion basica de archivos completada.

--- TareaProgramada2/TareaProgramada2/Source.cpp
+++ TareaProgramada2/TareaProgramada2/Source.cpp
@@ -1,12 +1,82 @@
 #include "Ventana.h"
-#include "GUI.h"
-#include "Graph.h"
+#include "tablahash.h"
+#include <fstream>
+ifstream archivo;
+ofstream archivoO;
+HashMap  * tablaHash = new HashMap();
 
+bool insertar(string valor) {
+	if (tablaHash->verificarLlave(split(valor,';').at(0))) {
+		return false;
+	}
+	archivoO.open("data.txt");
+	archivoO << valor << endl;
+	tablaHash->put(valor);
+	archivoO.close();
+	return true;
+}
+bool eliminar(string key) {
+	if (!tablaHash->verificarLlave(key)) {
+		return false;
+	}
+	archivo.open("data.txt");
+	archivoO.open("temp.txt");
+	string linea;
+	while (getline(archivo, linea)) {
+		if (split(linea, ';').at(0) != key) {
+			archivoO << linea << endl;
+		}
+	}
+	tablaHash->remove(key);
+	archivo.close();
+	archivoO.close();
+	remove("data.txt");
+	rename("temp.txt", "data.txt");
+	return true;
+}
+bool modificar(string valor) {
+	vector<string> vec = split(valor, ';');
+	if (!tablaHash->verificarLlave(vec.at(0))) {
+		return false;
+	}
+	tablaHash->replace(valor);
+	archivo.open("data.txt");
+	archivoO.open("temp.txt");
+	string linea;
+	while (getline(archivo, linea)) {
+		if (split(linea,';').at(0) != split(valor,';').at(0)) {
+			archivoO << linea << endl;
+		}
+		else {
+			archivoO << valor << endl;
+		}
+	}
+	archivo.close();
+	archivoO.close();
+	return true;
+}
+bool consultar(string key) {
+	string consulta = tablaHash->get(key);
+	if (consulta == "") {
+		return false;
+	}
+	vector<string> vec = split(consulta, ';');
+	string nombre = vec.at(1);
+	string primerA = vec.at(2);
+	string segundoA = vec.at(3);
+	string fecha = vec.at(4);
+	return true;
+}
 int main() {
 	using namespace Graph_lib;
-	Ventana win(Point(200,200),600,500,"Simple Window");
-
-	win.wait_for_button();
-	
+	archivo.open("data.txt");
+	if (archivo.fail()) {
+		return -1;
+	}
+	string info;
+	while (getline(archivo, info)) {
+		tablaHash->put(info);
+	}
+	archivo.close();
 	return 0;
 }
---
+++ TareaProgramada2/TareaProgramada2/tablahash.h
@@ -0,0 +1,121 @@
+#include <string>
+#include <vector>
+using namespace std;
+vector<string> split(string cadena, char divisor) {
+	vector<string> vec;
+	string palabra;
+	for (int i = 0; i < cadena.size(); ++i) {
+		if (cadena[i] == divisor) {
+			vec.push_back(palabra);
+			palabra = "";
+		}
+		else {
+			palabra += cadena[i];
+		}
+	}
+	vec.push_back(palabra);
+	return vec;
+}
+class HashNode {
+public:
+	HashNode(string key, string nombre, string primerA, string segundoA,string fecha) :
+		key(key), nombre(nombre), primerA(primerA),segundoA(segundoA),fecha(fecha){
+	}
+
+	string getKey() {
+		return key;
+	}
+
+	string getNombre() {
+		return nombre;
+	}
+	string getPrimerA() {
+		return primerA;
+	}
+	string getSegundoA() {
+		return segundoA;
+	}
+	string getFecha() {
+		return fecha;
+	}
+
+
+private:
+	// key-value pair
+	string key;
+	string nombre;
+	string primerA;
+	string segundoA;
+	string fecha;
+};
+
+class HashMap{
+public:
+	HashMap() {
+	}
+
+	~HashMap() {
+		// destroy all buckets one by one
+		for (int i = 0; i < table.size(); i++) {
+			delete(table.at(i));
+		}
+	}
+	bool verificarLlave(string key) {
+		for (int i = 0; i < table.size(); i++) {
+			if (table.at(i)->getKey == key) {
+				return true;
+			}
+		}
+		return false;
+	}
+	string get(string key) {
+		string final = "";
+		for (int i = 0; i < table.size();i++) {
+			if (table.at(i)->getKey() == key) {
+				final += table.at(i)->getKey() + ";";
+				vector<string> vec = split(table.at(i)->getNombre(), ' ');
+				for (int j = 0; j < vec.size(); i++) {
+					final += vec.at(j) + ";";
+				}
+				final += table.at(i)->getPrimerA() +";";
+				final += table.at(i)->getSegundoA() + ";";
+				final += table.at(i)->getFecha();
+				break;
+			}
+		}
+		return final;
+	}
+	bool put(string valor) {
+		vector<string> vec = split(valor, ';');
+		if (verificarLlave(vec.at(0))) {
+			return false;
+		}
+		HashNode * nuevo = new HashNode(vec.at(0), vec.at(1), vec.at(2), vec.at(3), vec.at(4));
+		table.push_back(nuevo);
+		return true;
+	}
+	bool replace(string valor) {
+		vector<string> vec = split(valor, ';');
+		for (int i = 0; i < table.size(); i++) {
+			if (table.at(i)->getKey == vec.at(0)) {
+				HashNode * nuevo = new HashNode(vec.at(0), vec.at(1), vec.at(2), vec.at(3), vec.at(4));
+				delete(table.at(i));
+				table.at(i) = nuevo;
+				return true;
+			}
+		}
+		return false;
+	}
+	bool remove(string key) {
+		for (int i = 0; i < table.size(); i++) {
+			if (table.at(i)->getKey == key) {
+				table.erase(table.begin() + i);
+				return true;
+			}
+		}
+		return false;
+	}
+
+private:
+	vector<HashNode*> table;
+};
